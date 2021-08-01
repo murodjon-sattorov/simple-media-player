73125dfaffd976e32f85944d1195a040f48c0e83
Murodjon <sattorovmurodjon43@gmail.com>
2021-08-01 21:49:38 +0500

    simple mediaplayer and add visualizer

--- app/src/main/java/uz/murodjon_sattorov/simplemusicplayer/visualizer/model/AnimSpeed.kt
+++ app/src/main/java/uz/murodjon_sattorov/simplemusicplayer/visualizer/model/AnimSpeed.kt
@@ -1,14 +1,12 @@
-package uz.murodjon_sattorov.simplemusicplayer.visualizer.model;
+package uz.murodjon_sattorov.simplemusicplayer.visualizer.model
 
 /**
- * Created by <a href="mailto:sattorovmurodjon43@gmail.com">Murodjon Sattorov</a>
+ * Created by [Murodjon Sattorov](mailto:sattorovmurodjon43@gmail.com)
  *
  * @author Murodjon
  * @date 01.08.2021
  * @project Simple music player
- **/
-public enum AnimSpeed {
-    SLOW,
-    MEDIUM,
-    FAST
+ */
+enum class AnimSpeed {
+    SLOW, MEDIUM, FAST
 }
--- app/src/main/java/uz/murodjon_sattorov/simplemusicplayer/visualizer/model/PaintStyle.kt
+++ app/src/main/java/uz/murodjon_sattorov/simplemusicplayer/visualizer/model/PaintStyle.kt
@@ -1,13 +1,12 @@
-package uz.murodjon_sattorov.simplemusicplayer.visualizer.model;
+package uz.murodjon_sattorov.simplemusicplayer.visualizer.model
 
 /**
- * Created by <a href="mailto:sattorovmurodjon43@gmail.com">Murodjon Sattorov</a>
+ * Created by [Murodjon Sattorov](mailto:sattorovmurodjon43@gmail.com)
  *
  * @author Murodjon
  * @date 01.08.2021
  * @project Simple music player
- **/
-public enum PaintStyle {
-    OUTLINE,
-    FILL
+ */
+enum class PaintStyle {
+    OUTLINE, FILL
 }
